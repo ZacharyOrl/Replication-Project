ffa416419455a9c19790e311c228b9113ddd43a1
Zachary Orlando <zacharyorlando@gmail.com>
2025-04-17 21:31:49 -0500

    Finished the required files and small updates to model solver

---
+++ compute_initial_dist.jl
@@ -0,0 +1,19 @@
+############################################################
+# Discretize a normal initial distribution onto a grid. 
+############################################################
+# Inputs: μ_0 - mean of the initial distribution, σ_0 - variance of the initial distribution, grid - a vector of grid points. 
+# Outputs: dist - a vector of masses associated with each grid point. 
+function compute_initial_dist(μ_0::Float64,σ_0::Float64, grid::Vector{Float64})
+    n = length(grid) 
+
+    dist =zeros(n)
+    dist[1] = cdf(Normal(μ_0, σ_0), grid[1])
+
+    for i = 2:n-1
+        dist[i] = cdf(Normal(μ_0, σ_0), grid[i]) - cdf(Normal(μ_0, σ_0), grid[i - 1])
+    end 
+
+    dist[n] = 1 - cdf(Normal(μ_0, σ_0), grid[n - 1])
+
+    return dist
+end
--- portfolio_choice_with_housing.jl
+++ portfolio_choice_with_housing.jl
@@ -1,5 +1,5 @@
 ###########################################
-# Estimates the Lifecycle Portfolio Choice Model of 
+# Solves the Lifecycle Portfolio Choice Model of 
 # Cocco (2005, RFS) "Portfolio Choice in the Presence of Housing"
 ###########################################
 # Packages 
@@ -30,63 +30,74 @@ using Parameters
 @with_kw struct Model_Parameters
     # Parameters with type annotations
     # Variance Parameters 
-    σ_η::Float64 = 0.019^2
-    σ_ι::Float64 = 0.1674^2
-    σ_ω::Float64 = 0.136^2 # Start with the no-college case 
-    σ_p::Float64 = 0.062^2 
+    σ_η::Float64 = 0.019^2  # Variance of persistent component of earnings
+    σ_ι::Float64 = 0.1674^2 # Variance of stock market innovation
+    σ_ω::Float64 = 0.136^2  # Start with the no-college case 
+    σ_p::Float64 = 0.062^2  # Variance of house prices
 
     # Correlations between processes 
-    κ_ω::Float64 = 0.00
-    κ_η::Float64 = σ_η/σ_p
-    ρ_ϵ_ι::Float64 = 0.0
-    φ::Float64 = 0.748
+    κ_ω::Float64 = 0.00     # Correlation between house prices and transitory component
+    κ_η::Float64 = σ_η/σ_p  # Regression coefficient of cyclical fluctuations in house prices on persistent component (correlation is 1)
+    ρ_ϵ_ι::Float64 = 0.0    # Correlation between persistent component of income and the stock market
+    φ::Float64 = 0.748      # Autocorrelation in the persistent component 
 
     # One time stock market entry cost 
     F::Float64 = 1000.0
 
     # Returns / interest rates 
-    R_D::Float64 = compound(0.04, 5)
-    R_F::Float64 = compound(0.02, 5)
-    R_S::Float64 = 0.1
-    μ::Float64 = compound(log(R_S + 1.0) - σ_η/2, 5)
+    R_D::Float64 = compound(0.04, 5)                  # Mortgage interest rate 
+    R_F::Float64 = compound(0.02, 5)                  # Risk-free rate
+    R_S::Float64 = 0.1                                # Expected return on stocks 
+    μ::Float64 = compound(log(R_S + 1.0) - σ_η/2, 5)  # Expected log-return on stocks
 
     # Housing parameters
-    d::Float64 = 0.15
-    π::Float64 = 0.244
-    δ::Float64 = 0.01
-    λ::Float64 = 0.08
-    b::Float64 = compound(0.01, 5)
+    d::Float64 = 0.15              # Down-Payment proportion 
+    π::Float64 = 0.244             # Moving shock probability 
+    δ::Float64 = 0.01              # Housing Depreciation
+    λ::Float64 = 0.08              # House-sale cost 
+    b::Float64 = compound(0.01, 5) # Real house price growth over 5 years  - matching the way it is presented in the paper 
 
     # Utility function parameters
-    θ::Float64 = 0.1
-    γ::Float64 = 5.0
-    β::Float64 = compound(0.96,5)
+    θ::Float64 = 0.1              # Utility from housing services relative to consumption
+    γ::Float64 = 5.0              # Risk-aversion parameter 
+    β::Float64 = compound(0.96,5) # Discount Rate 
 
     # Lifecycle Parameters 
-    T::Int64 = 10
-    TR::Int64 = 9
+    T::Int64 = 10 # Each T represents five years of life from 25 to 75
+    TR::Int64 = 9 # The final two time periods represent retirement 
 
-    # Grids
+    # Grids & Transition Matrices
+    # Persistent Earnings
     η_grid::Vector{Float64} = rouwenhorst(σ_η, φ, 3)[1]
     T_η::Matrix{Float64} = rouwenhorst(σ_η, φ, 3)[2]
+    nη::Int64 = length(η_grid)
+
+    # Transitory Earnings
     ω_grid::Vector{Float64} = rouwenhorst(σ_ω, 0.0, 3)[1]
     T_ω::Matrix{Float64} = rouwenhorst(σ_ω, 0.0, 3)[2]
-
     nω::Int64 = length(ω_grid)
-    nη::Int64 = length(η_grid)
-    η_0::Float64 = 0.0
 
+    # In the paper, there is no initial level of housing and LW_1 = 0 
+    # But what is η_0? He doesn't say... 
+    η_0::Float64 = 0.0 # For now, assume that persistent earnings starts at the unconditional mean
+
+    # Deterministic earnings
+    # For now, use what my own estimates from the PSID. 
     κ::Matrix{Float64} = hcat(CSV.File("life_cycle_income.csv").age, CSV.File("life_cycle_income.csv").deterministic_component)
 
+    # Stock market grids
     ι_grid::Vector{Float64} = rouwenhorst(σ_ι, 0.0, 3)[1]
     T_ι::Matrix{Float64} = rouwenhorst(σ_ι, 0.0, 3)[2]
     nι::Int64 = length(ι_grid)
 
+    # Housing grids
     p_grid::Vector{Float64} = κ_η .* η_grid
-    P_bar::Float64 = 30500.0 * 140.3/41.8
+    P_bar::Float64 = 30500.0 * 140.3/41.8 # For now, use an estimate of house prices in 1972 and inflate using the ratio of CPI in 1992 to CPI in 1972
+                                          # 1972 house prices taken from https://www.huduser.gov/periodicals/ushmc/winter2001/histdat08.htm 
+                                          # CPI taken from https://fred.stlouisfed.org/series/CPIAUCSL 
     np::Int64 = nη
 
-    # States 
+    # State / Choice Grids 
     c_min::Float64 = 0.0001
     c_max::Float64 = 1000000
     nc::Int64 = 20
@@ -132,8 +143,6 @@ function build_solutions(para)
 
     # Last two fields represent whether the agent has to move and whether they have previously paid the stock market entry fee
     val_func = zeros(Float64,para.T,para.nH,para.nX,para.nη,2,2 ) 
-
-    # Last field represents whether the agent chooses to pay the stock market entry fee
     c_pol_func  = zeros(Float64,para.T,para.nH,para.nX,para.nη,2,2 ) 
     H_pol_func  = zeros(Float64,para.T,para.nH,para.nX,para.nη,2,2 ) 
     D_pol_func  = zeros(Float64,para.T,para.nH,para.nX,para.nη,2,2 ) 
@@ -162,10 +171,8 @@ function flow_utility_func(c::Float64, H::Float64, para::Model_Parameters)
     return (   ( c^(1-θ) * H^θ )^( 1 - γ )   ) / (1 - γ)
 end 
 
-# Takes states X, H_t, η, Inv Move, IFC, 
-# choices C, H_prime,  D, α, FC 
-# and random variables ω_prime, r_prime, η_prime 
-# and outputs the sum of stocks and bonds
+# Takes as input all states and choices necessary to pin down the budget constraint
+# and outputs the sum of stocks and bonds (LHS of the budget constraint in Cocco)
 function budget_constraint(X::Float64, H::Float64, P::Float64, Inv_Move::Int64,c::Float64, 
                            H_prime::Float64, D::Float64, FC::Float64, para::Model_Parameters)
     @unpack_Model_Parameters para
@@ -188,8 +195,8 @@ function debt_constraint(D::Float64, H::Float64, P::Float64,para::Model_Paramete
     return (1-d) * H_prime * P - D
 end 
 
+# Given realized wealth in T+1, computes the bequest agent's bequest utility.    
 function compute_bequest_value(V::Array{Float64,5}, para::Model_Parameters)
-    # Given a realized state in T+1 and a , computes the bequest agent's bequest utility.    
     @unpack_Model_Parameters para 
 
     # Loop over Housing States 
@@ -218,9 +225,10 @@ function compute_bequest_value(V::Array{Float64,5}, para::Model_Parameters)
     return V
 end 
 
+# Creates a linear interpolation function using a mapping from grid x1 to outcome F. 
+# Cubic-spline interpolation within-grid
+# Allows for extrapolation outside the grid (Flat extrapolation)
 function linear_interp(F::Array{Float64, 1}, x1::Vector{Float64})
-
-    # need to get range object for Interpolations.jl
     x1_grid = range(minimum(x1), maximum(x1), length=length(x1))
 
     interp = interpolate(F, BSpline(Cubic()))
@@ -228,8 +236,8 @@ function linear_interp(F::Array{Float64, 1}, x1::Vector{Float64})
     return  extrap
 end
 
+# Solves the decision problem, outputs results back to the sols structure. 
 function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
-    # Solves the decision problem, outputs results back to the sols structure. 
     @unpack_Model_Parameters para 
     @unpack val_func, c_pol_func, H_pol_func, D_pol_func, FC_pol_func, α_pol_func = sols
 
@@ -241,12 +249,12 @@ function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
 
     println("Begin solving the model backwards")
     for j in T:-1:TR  # Backward induction
+
         println("Solving the Retiree's Problem")
         println("Age is ", 25 + 5*j)
         
-        # Generate interpolation functions for each possible cash on hand tomorrow 
+        # Generate interpolation functions for cash-on hand given each possible combination of the other states tomorrow 
         interp_functions = Vector{Any}(undef, nH * nη * 2 * 2)
-
         for H_index in 1:nH
             for η_prime_index in 1:nη
                 for Inv_Move in 0:1
@@ -297,16 +305,17 @@ function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
                                                 α_index = 1 
                                                 α = α_grid[α_index]
 
+                                                # Skip if implied stock and bond spending must be negative
                                                 if budget_constraint(X, H, P, Inv_Move, c, H_prime, D, FC, para) <= 0
                                                     continue
                                                 end
 
+                                                # Skip if debt exceeds the collateral constraint. 
                                                 if debt_constraint(D, H, P) <= 0
                                                     continue 
                                                 end  
 
                                                 val = flow_utility_func(c, H)
-
                                                 # Find the continuation value 
                                                 # Loop over random variables 
                                                 for η_prime_index in 1:nη
@@ -317,6 +326,7 @@ function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
                                                         for ι_prime_index in 1:nr 
                                                             ι_prime = ι_grid[ι_prime_index]
 
+                                                            R_prime = exp(ι_prime + μ)
                                                             Y_Prime = exp(η_prime + ω_prime + κ[T, 2])
                                                             P_Prime = P_bar + exp(b * (T-1) + p[η_prime_index])
 
@@ -325,34 +335,39 @@ function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
                                                             B = (1-α) * S_and_B 
 
                                                             # Compute next period's liquid wealth
-                                                            X_prime = ι_prime * S + R_F * B - R_D * D + Y_Prime
+                                                            X_prime = R_prime * S + R_F * B - R_D * D + Y_Prime
 
-                                                            val += (1-π) * Prob(η_index, η_prime_index, ω_index, ω_prime_index, ι_index, ι_prime_index) *
+                                                            val += (1-π) * T_η[η_index, η_prime_index] * T_ω[1, ω_prime_index] * T_ι[1, ι_prime_index] *                                # Not forced to Move 
                                                                    interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (0 * 2) + FC + 1](X_prime) +
-                                                                   π * Prob(η_index, η_prime_index, ω_index, ω_prime_index, ι_index, ι_prime_index) *
+                                                                   π     * T_η[η_index, η_prime_index] * T_ω[1, ω_prime_index] * T_ι[1, ι_prime_index] *                                # Forced to move
                                                                    interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (1 * 2) + FC + 1](X_prime)
-
-                                                            # Update value function
-                                                            if val > candidate_max 
-                                                                val_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = val
-                                                                c_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = c 
-                                                                H_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = H_prime
-                                                                D_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = D
-                                                                FC_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = 0
-                                                                α_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = α
-                                                            end 
                                                         end 
                                                     end 
                                                 end 
+                                                
+                                                # Update value function
+                                                if val > candidate_max 
+                                                    val_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]    = val
+                                                    
+                                                    c_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = c 
+                                                    H_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = H_prime
+                                                    D_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = D
+                                                    FC_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = FC
+                                                    α_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = α
+                                                    
+                                                end 
+                                            # If !(IFC == 0 && FC == 0)
                                             else 
                                                 # Loop over Risky-share choices
                                                 for α_index in 1:nα 
                                                     α = α_grid[α_index]
 
+                                                    # Skip if implied stock and bond spending must be negative
                                                     if budget_constraint(X, H, P, Inv_Move, c, H_prime, D, FC, para) <= 0
                                                         continue
                                                     end
 
+                                                    # Skip if debt exceeds the collateral constraint. 
                                                     if debt_constraint(D, H, P) <= 0
                                                         continue 
                                                     end 
@@ -379,23 +394,23 @@ function Solve_Worker_Problem(para::Model_Parameters, sols::Solutions)
                                                                 # Compute next period's liquid wealth
                                                                 X_prime = ι_prime * S + R_F * B - R_D * D + Y_Prime
 
-                                                                val += (1-π) * Prob(η_index, η_prime_index, ω_index, ω_prime_index, ι_index, ι_prime_index) *
-                                                                       interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (0 * 2) + FC + 1](X_prime) +
-                                                                       π * Prob(η_index, η_prime_index, ω_index, ω_prime_index, ι_index, ι_prime_index) *
-                                                                       interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (1 * 2) + FC + 1](X_prime)
-
-                                                                # Update value function
-                                                                if val > candidate_max 
-                                                                    val_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = val
-
-                                                                    c_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = c 
-                                                                    H_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = H_prime
-                                                                    D_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = D
-                                                                    FC_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = FC
-                                                                    α_pol_func[H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = α
-                                                                end 
+                                                                
+                                                                val += (1-π) * T_η[η_index, η_prime_index] * T_ω[1, ω_prime_index] * T_ι[1, ι_prime_index] *
+                                                                    interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (0 * 2) + FC + 1](X_prime) +
+                                                                     π * T_η[η_index, η_prime_index] * T_ω[1, ω_prime_index] * T_ι[1, ι_prime_index] *
+                                                                    interp_functions[((H_prime_index - 1) * nη * 2 * 2) + ((η_prime_index - 1) * 2 * 2) + (1 * 2) + FC + 1](X_prime)
                                                             end 
-                                                        end
+                                                        end 
+                                                    end 
+                                                    # Update value function
+                                                    if val > candidate_max 
+                                                        val_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]    = val
+
+                                                        c_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = c 
+                                                        H_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = H_prime
+                                                        D_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = D
+                                                        FC_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1] = FC
+                                                        α_pol_func[j, H_index, X_index, η_index, Inv_Move + 1, IFC + 1]  = α
                                                     end 
                                                 end 
                                             end 
